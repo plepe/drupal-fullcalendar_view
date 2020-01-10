be6c4524273899a7d1591bc25de09063b959a771
AMDS <AMDS@2986445.no-reply.drupal.org>
2020-01-10 11:13:20 +1100

    Issue #2987930 by NickDJM, minorOffense, Mingsong, caspervoogt: View calendar in page's current language

--- src/Plugin/FullcalendarViewProcessor/CurrentLanguage.php
+++ src/Plugin/FullcalendarViewProcessor/CurrentLanguage.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\fullcalendar_view\Plugin\FullcalendarViewProcessor;
 
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\fullcalendar_view\Plugin\FullcalendarViewProcessorBase;
 
 /**
@@ -17,12 +18,13 @@ class CurrentLanguage extends FullcalendarViewProcessorBase {
     $view = $variables['view'];
     $style = $view->style_plugin;
     $options = $style->options;
+    // Field machine name for event description.
+    $des_field = $options['des'];
     
     // Update the default language, only if it is specified as current active language.
     if ($options['defaultLanguage'] === 'current_lang') {
       // Get current language.
       $language = \Drupal::languageManager()->getCurrentLanguage();
-      
       $default_lang = $this->fullcalendar_view_map_langcodes($language->getId());
       $entries = isset($variables['#attached']['drupalSettings']['fullCalendarView']) ? $variables['#attached']['drupalSettings']['fullCalendarView'] : [];
       
@@ -35,6 +37,44 @@ class CurrentLanguage extends FullcalendarViewProcessorBase {
         foreach ($entries as &$entry) {
           if ($entry['id'] === $current_entity->id()) {
             $entry['url'] = $current_entity->toUrl('canonical', ['language' => $language])->toString();
+            // Translate the title and description,
+            // if using the entity field rather than
+            // the raw view fields.
+            if ($options['use_entity_fields']) {
+              if ($current_entity->hasTranslation($language->getId())) {
+                $translated_entity = $current_entity->getTranslation($language->getId());
+              }
+              else {
+                // Current entity doesn't have the translation for
+                // the language specified.
+                $translated_entity = $current_entity->getTranslation(LanguageInterface::LANGCODE_DEFAULT);
+              }
+
+              $entry['title'] = $translated_entity->getTitle();
+              // Description for events. For multiple bundle types,
+              // there might be more than one field specified.
+              if (!empty($des_field) && is_array($des_field)) {
+                foreach ($des_field as $des_field_name) {
+                  if ($current_entity->hasField($des_field_name)) {
+                    $des = $translated_entity->get($des_field_name)->value;
+                    // We just need only one description text.
+                    // Once we got it, quit the loop.
+                    break;
+                  }
+                }
+              }
+              if (!isset($des)) {
+                $des = '';
+              }
+              if (is_array($des)) {
+                $des = reset($des);
+                if (isset($des['value'])) {
+                  $des = $des['value'];
+                }
+              }
+              $entry['description'] = $des;
+            }
+
             break;
           }
         }
